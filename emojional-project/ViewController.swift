f084aa5b8b4e52c8e99d1931de9e992311f36cb2
aileencastro <aileencastrogg@gmail.com>
2019-07-12 09:45:31 -0700

    Complete Iteration 1

---
+++ emojional-project/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  emojional-project
+//
+//  Created by Apple on 7/12/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Warning:", message: "This petri dish contains lice.", preferredStyle: UIAlertController.Style.alert)
+        // in as much detail as possible
+        // pseudo-code the things that need to happen
+        // when a user clicks an emoji button
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+
+
+
+}
+
